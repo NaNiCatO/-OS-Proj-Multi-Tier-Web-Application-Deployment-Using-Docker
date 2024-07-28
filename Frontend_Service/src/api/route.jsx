77537fd08085366758c9f7c010656dd78b287ded
SSYino <gianmario.nino@gmail.com>
2024-07-29 01:34:48 +0700

    Add axios headers

--- Frontend_Service/src/api/route.jsx
+++ Frontend_Service/src/api/route.jsx
@@ -1,10 +1,27 @@
 import axios from 'axios';
 
+// axios.defaults.baseURL = 'http://localhost:5001';
+axios.defaults.headers.post['Content-Type'] ='application/json;charset=utf-8'
+axios.defaults.headers.post['Access-Control-Allow-Origin'] = '*';
+
 export const ProductService = {
-  getProducts() {
-    return axios.get('http://backend:5000/all_products')
-      .then(res => res.data)
-      .catch(error => { throw error });
+  async getProducts() {
+    try {
+      const response = await axios.get('http://localhost:5001/products'
+        ,{
+        headers: {
+          'Access-Control-Allow-Origin': '*',
+          'Content-Type': 'application/json'
+        }}
+        );
+      return response.data
+    } catch (error) {
+      console.error('Error fetching products:', error);
+      throw error
+    }
+    // return axios.get('http://localhost:5001/products')
+    //   .then(res => res.data)
+    //   .catch(error => { throw error });
   },
   searchProducts(name, price, order_by, category, available) {
     return axios.get(`http://backend:5000/products?name=${name}&price=${price}&order_by=${order_by}&category=${category}&available=${available}`)
